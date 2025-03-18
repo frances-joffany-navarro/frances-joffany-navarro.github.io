ca46caee623e75c93d41dfff1c0dff3e0f466851
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-03-18 22:43:34 +0100

    integrate typed.js, adjust headline position

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 /* Initialize Tooltip Component */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+/* Setup Type.js animation */
+var typed = new Typed('.auto-type', {
+  strings: ['Web Developer','Lives in Brussels, Belgium', 'Practise DevOps Integration'],
+  typeSpeed: 150,
+  backSpeed: 50,
+  loop: true
+});
